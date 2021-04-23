58c1d855ad9cba3fde6c963cb44655b68f20f5ca
Gerardo Larios <gerardo.larios69@gmail.com>
2021-04-23 16:28:02 -0500

    [ADD] recursive solution

--- solution.js
+++ solution.js
@@ -1,4 +1,11 @@
-module.exports.sumarize = function(array = []) {
-  if (array === null) return 0
-  return array.reduce((acc, item) => acc + item, 0)
+function sumarize(array = [], acc = 0, index = 0) {
+  if (array === null || array?.length <= index) return acc
+
+  const item = array[index]
+
+  return sumarize(array, item + acc, index + 1)
 }
+
+// O^n
+
+module.exports.sumarize = sumarize
